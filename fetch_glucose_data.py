b53163c740da33a6275a23d430a378ae4fdef750
remi3297 <146122656+remi3297@users.noreply.github.com>
2024-06-09 19:21:32 +0200

    Update fetch_glucose_data.py

--- fetch_glucose_data.py
+++ fetch_glucose_data.py
@@ -3,10 +3,16 @@ import json
 import datetime
 import os
 
-LIBRELINKUP_EMAIL = os.getenv('LIBRELINKUP_EMAIL', 'remi.lecussan@yahoo.fr')
-LIBRELINKUP_PASSWORD = os.getenv('LIBRELINKUP_PASSWORD', 'Remi32971997!')
+# Vos informations d'identification
+LIBRELINKUP_EMAIL = os.getenv('LIBRELINKUP_EMAIL')
+LIBRELINKUP_PASSWORD = os.getenv('LIBRELINKUP_PASSWORD')
 NIGHTSCOUT_URL = os.getenv('NIGHTSCOUT_URL')
 NIGHTSCOUT_API_SECRET = os.getenv('NIGHTSCOUT_API_SECRET')
+
+# Informations du proxy
+PROXY_URL = os.getenv('PROXY_URL')
+PROXY_USERNAME = os.getenv('PROXY_USERNAME')
+PROXY_PASSWORD = os.getenv('PROXY_PASSWORD')
 
 print("Starting fetch_glucose_data.py script")
 
@@ -14,6 +20,13 @@ print(f"LIBRELINKUP_EMAIL: {LIBRELINKUP_EMAIL}")
 print(f"LIBRELINKUP_PASSWORD: {'*' * len(LIBRELINKUP_PASSWORD) if LIBRELINKUP_PASSWORD else None}")
 print(f"NIGHTSCOUT_URL: {NIGHTSCOUT_URL}")
 print(f"NIGHTSCOUT_API_SECRET: {'*' * len(NIGHTSCOUT_API_SECRET) if NIGHTSCOUT_API_SECRET else None}")
+
+proxies = {
+    'http': PROXY_URL,
+    'https': PROXY_URL,
+}
+
+auth = (PROXY_USERNAME, PROXY_PASSWORD)
 
 def get_librelinkup_session():
     login_url = 'https://api.libreview.io/llu/auth/login'
@@ -25,18 +38,26 @@ def get_librelinkup_session():
         'Content-Type': 'application/json',
         'User-Agent': 'FreeStyle LibreLink Up/4.7.0 (iOS; 15.2; iPhone; en_US)',
         'version': '4.7.0',
-        'product': 'llu.ios',
-        'Accept-Language': 'en-US'
+        'product': 'llu.ios'
     }
 
     try:
-        response = requests.post(login_url, data=json.dumps(payload), headers=headers)
+        response = requests.post(login_url, data=json.dumps(payload), headers=headers, proxies=proxies, auth=auth)
         print(f"Initial Response Status Code: {response.status_code}")
         print(f"Initial Response Text: {response.text}")
         response.raise_for_status()
         data = response.json()
 
-        # Ajout d'un journal pour afficher le contenu de la réponse JSON
+        if 'redirect' in data['data'] and data['data']['redirect']:
+            region = data['data']['region']
+            regional_login_url = f'https://{region}.api.libreview.io/llu/auth/login'
+            print(f"Redirecting to regional URL: {regional_login_url}")
+            response = requests.post(regional_login_url, data=json.dumps(payload), headers=headers, proxies=proxies, auth=auth)
+            print(f"Redirected Response Status Code: {response.status_code}")
+            print(f"Redirected Response Text: {response.text}")
+            response.raise_for_status()
+            data = response.json()
+
         print(f"Response JSON: {json.dumps(data, indent=2)}")
 
         auth_ticket = data['data'].get('authTicket')
@@ -59,10 +80,9 @@ def get_glucose_data(session_token):
         'Content-Type': 'application/json',
         'User-Agent': 'FreeStyle LibreLink Up/4.7.0 (iOS; 15.2; iPhone; en_US)',
         'version': '4.7.0',
-        'product': 'llu.ios',
-        'Accept-Language': 'en-US'
+        'product': 'llu.ios'
     }
-    response = requests.get(data_url, headers=headers)
+    response = requests.get(data_url, headers=headers, proxies=proxies, auth=auth)
     print(f"Glucose Data Response Status Code: {response.status_code}")
     print(f"Glucose Data Response Text: {response.text}")
     response.raise_for_status()
@@ -82,7 +102,7 @@ def send_to_nightscout(glucose_data):
                 'API-SECRET': NIGHTSCOUT_API_SECRET,
                 'Content-Type': 'application/json'
             }
-            response = requests.post(f'{NIGHTSCOUT_URL}/api/v1/entries', data=json.dumps(payload), headers=headers)
+            response = requests.post(f'{NIGHTSCOUT_URL}/api/v1/entries', data=json.dumps(payload), headers=headers, proxies=proxies, auth=auth)
             response.raise_for_status()
             print(f"Successfully sent data to Nightscout: {payload}")
 
@@ -96,11 +116,4 @@ if __name__ == '__main__':
             print(f"Connection ID: {connection['id']}")
             if 'glucoseMeasurement' in connection:
                 glucose_measurement = connection['glucoseMeasurement']
-                print(f"Date: {glucose_measurement['Timestamp']}, Glucose Value: {glucose_measurement['Value']}")
-                send_to_nightscout([connection])
-            else:
-                print("No glucose measurement data available.")
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
-    except Exception as err:
-        print(f"An error occurred: {err}")
+                print(f"Date: {glucose_measurement['Timestamp']}, Glucose Value: {glucose_measurement['
