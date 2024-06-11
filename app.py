da99959d059387edd88176f5daa133131d7b55c6
remi3297 <146122656+remi3297@users.noreply.github.com>
2024-06-11 20:50:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import json
 import os
 
-# Extraction des informations de connexion des variables d'environnement
+# Chargement des variables d'environnement pour les identifiants
 LIBRELINKUP_EMAIL = os.getenv('LIBRELINKUP_EMAIL')
 LIBRELINKUP_PASSWORD = os.getenv('LIBRELINKUP_PASSWORD')
 
@@ -12,6 +12,7 @@ app = Flask(__name__)
 scheduler = BackgroundScheduler()
 
 def get_librelinkup_session():
+    print("Tentative de connexion à LibreLinkUp...")
     login_url = 'https://api.libreview.io/llu/auth/login'
     payload = {'email': LIBRELINKUP_EMAIL, 'password': LIBRELINKUP_PASSWORD}
     headers = {
@@ -21,11 +22,17 @@ def get_librelinkup_session():
         'product': 'llu.ios'
     }
     response = requests.post(login_url, data=json.dumps(payload), headers=headers)
+    if response.status_code == 200:
+        print("Connexion réussie.")
+    else:
+        print(f"Échec de la connexion, statut: {response.status_code}")
     response.raise_for_status()
     return response.json()['data']['authTicket']
 
 def fetch_glucose_data():
+    print("Récupération du token de session...")
     session_token = get_librelinkup_session()
+    print("Récupération des données de glycémie...")
     data_url = 'https://api.libreview.io/llu/connections'
     headers = {
         'authorization': f'Bearer {session_token["token"]}',
@@ -35,6 +42,10 @@ def fetch_glucose_data():
         'product': 'llu.ios'
     }
     response = requests.get(data_url, headers=headers)
+    if response.status_code == 200:
+        print("Données récupérées avec succès.")
+    else:
+        print(f"Échec de la récupération des données, statut: {response.status_code}")
     response.raise_for_status()
     return response.json()['data']
 
@@ -42,16 +53,21 @@ latest_data = []
 
 @app.route('/glucose')
 def glucose():
+    print("Demande de données de glycémie reçue...")
     return jsonify(latest_data)
 
 @scheduler.scheduled_job('interval', minutes=1)
 def timed_job():
+    print("Exécution de la tâche planifiée...")
     global latest_data
     try:
         latest_data = fetch_glucose_data()
+        print("Mise à jour des données réussie.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"Une erreur est survenue lors de la mise à jour des données: {e}")
 
 if __name__ == '__main__':
+    print("Démarrage du scheduler...")
     scheduler.start()
+    print("Démarrage du serveur Flask...")
     app.run(host='0.0.0.0', port=5000)
