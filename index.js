bf998e6a12c9ae630b15d035a72e7132a186d07b
remi3297 <146122656+remi3297@users.noreply.github.com>
2024-07-25 18:21:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.get('/glucose', (req, res) => {
     } else if (modifiedValue >= 120 && modifiedValue <= 160) {
       modifiedValue += 20;
     }
-    res.json({ value: modifiedValue });
+    res.json({ value: 180 });
   } else {
     res.status(404).json({ error: 'No glucose data available' });
   }
